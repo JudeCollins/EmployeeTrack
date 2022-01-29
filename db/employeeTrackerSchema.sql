f6e5f6aca645a17b538184bb855574b4bdd49406
Jude Collins <jude.collins13@yahoo.com>
2022-01-29 12:10:01 -0500

    DB

--- db/employeeTrackerSchema.sql
+++ db/employeeTrackerSchema.sql
@@ -22,3 +22,15 @@ salary DECIMAL NOT NULL,
 department_id INT NOT NULL,
 FOREIGN KEY (department_id) REFERENCES department (id)
 );
+
+
+CREATE TABLE employee (
+
+id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+FOREIGN KEY (role_id) REFERENCES role (id),
+manager_id INTEGER NULL,
+FOREIGN KEY (manager_id) REFERENCES employee (id)
+);
