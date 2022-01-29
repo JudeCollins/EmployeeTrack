4a77618345d3b283bc1a4751bdb3f0f6c39ad3cc
Jude Collins <jude.collins13@yahoo.com>
2022-01-29 12:02:48 -0500

    packeges and starting db

---
+++ db/employeeTrackerSchema.sql
@@ -0,0 +1,7 @@
+-- delete the database if already exists
+DROP DATABASE IF EXISTS employee_trackerDB;
+-- Create database
+CREATE database employee_trackerDB;
+
+-- using  database
+USE employee_trackerDB;
