81672bf1e9ccd0382e4f23484de5f73e23477981
Jude Collins <jude.collins13@yahoo.com>
2022-01-29 12:53:20 -0500

    app.js

--- app.js
+++ app.js
@@ -264,6 +264,61 @@ function viewAll() {
       }
     );
   }
+
+  //adds employee
+  function addEmployee() {
+    connection.query(
+      "SELECT role.title, role.id FROM employee_trackerDB.role",
+      function (err, res) {
+        if (err) throw err;
+  
+        inquirer
+          .prompt([
+            {
+              name: "choice",
+              type: "list",
+              choices: function () {
+                var choiceArray = [];
+                for (var i = 0; i < res.length; i++) {
+                  choiceArray.push(res[i].title);
+                }
+                return choiceArray;
+              },
+              message: "Which Role?",
+            },
+          ])
+          .then(function (answer) {
+            console.log(answer);
+            console.log(answer.choice);
+  
+            var role_id = answer.choice;
+  
+            for (var i = 0; i < res.length; i++) {
+              if (res[i].title === answer.choice) {
+                role_id = res[i].id;
+                console.log(role_id);
+              }
+            }
+  
+            connection.query(
+              "INSERT INTO employee SET ?",
+              {
+                first_name: answer.first_name,
+                last_name: answer.last_name,
+                role_id: role_id,
+              },
+              function (err) {
+                if (err) throw err;
+  
+                console.log(`You have created an employee ${answer.first_name} ${answer.last_name} with a role of ${role_id}.`)
+  
+                questions();
+              }
+            );
+          });
+      }
+    );
+  }
   
   
   
