9783c14f88a1ae35cdf1f91855e11eaa9d957c07
Jude Collins <jude.collins13@yahoo.com>
2022-01-29 12:11:43 -0500

    DB

--- db/employeeTrackerSeed.sql
+++ db/employeeTrackerSeed.sql
@@ -1,3 +1,8 @@
 --initial database data
 
 INSERT INTO department (name) VALUES ("IT"),("Marketing"),("HR");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineer",100000,1),
+("Collaborator", 45000,2),
+("Recruiter",60000,3);
